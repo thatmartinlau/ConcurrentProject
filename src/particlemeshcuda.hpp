47c424a40754fd17a8c8947ec1d4ba986282143f
Oscar Peyron <oscar.peyron@allemagne.polytechnique.fr>
2025-05-18 22:58:24 +0200

    Add particlemeshcuda.hpp

---
+++ src/particlemeshcuda.hpp
@@ -0,0 +1,15 @@
+#ifndef PARTICLE_MESH_CUDA_H
+#define PARTICLE_MESH_CUDA_H
+
+#include "core.hpp"
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <iostream>
+#include <fftw3.h>
+#include <cufft.h>
+
+
+void particle_mesh_simulation_CUDA(System &universe, double dt, int grid_size, int num_bodies);
+
+#endif
